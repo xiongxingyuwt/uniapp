273390b0e572483e14645135bb9e18068a9c996b
xxy <1941461296@qq.com>
2020-07-03 15:21:45 +0800

    创建项目

---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="container">
+		<movable-area 
+			class="movable-area-box"
+		>
+			<movable-view 
+				class="move-view-box"
+				direction="horizontal"
+				:inertia="true"
+			>
+				<text class="movitem" :key="index" v-for="(item,index) in navlist">{{item}}</text>
+			</movable-view>
+		</movable-area>
+		<view class="">
+			<image src="../../static/images/bqk.png" mode=""></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				actstyle: 'rfs-80',
+				navlist:['热销','现货','酒饮','美护','餐厨','出行']
+			}
+		},
+		methods: {
+			clckactstyle(index){
+			}
+		}
+	}
+</script>
+
+<style>
+	.movable-area-box{
+		width: 100%;
+		height: 40px;
+	}
+	.move-view-box{
+		white-space: nowrap;
+	}
+	.movitem{margin: 0 30px;}
+	.movitem:hover{color:#FF0000}
+	.container{}
+</style>
---
+++ pages/myself/myself.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="myselfcontent">
+		<view class="topbg-box">
+			<image class="banner" src="../../static/images/tx.png" mode="aspectFit"></image>
+			<view class="photo-box">
+				<image class="temphone" src="../../static/images/tx.png" mode="center"></image>
+			</view>
+			<view class="userinfo">
+				<view class="uiusername fc3">莫妮卡盖勒</view>
+				<view class="uitemphone frd">156****9616</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniList from "@/components/uni-list/uni-list.vue"
+	import uniListItem from "@/components/uni-list-item/uni-list-item.vue"
+	export default {
+		components: {uniList,uniListItem},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.topbg-box{
+	position: relative;
+	border: 1px solid red;
+}
+.banner{
+	width: 100%;
+	top: -40px;
+}
+.photo-box{
+	position: absolute;
+	bottom: 87px;
+	left: 150px;
+	width: 122rpx;
+	height: 122rpx;
+	overflow: hidden;
+	border-radius: 50%;
+	.temphone{
+		width: 100%;
+		height: 100%;
+	}
+}
+.userinfo{
+	position: absolute;
+	bottom: 60rpx;
+	left: 0;
+	width: 100%;
+	text-align: center;
+	border: 1px solid red;
+}
+
+</style>
